de9b93718979479cba2cbebbf753cffeee786552
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-27 14:21:59 +0200

    fix comments

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@
    *utilizzando le properietà di Vue presenti in data.
    ?Bonus
      ?Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
-   *POST BONUS:
-    Chi invece è in regola con la consegna degli esercizi può sperimentare, in file separato,
-    tutto quello che abbiamo visto oggi!
+   
 */
 console.log("HERE",Vue);
 
