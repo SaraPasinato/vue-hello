225e63ff41d0b726c7e7ce6de52eda1bf26a6470
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-27 14:03:51 +0200

    add boilerplate

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1,
+   utilizzando le properietà di Vue presenti in data.
+   ?Bonus
+     Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
+   *POST BONUS:
+    Chi invece è in regola con la consegna degli esercizi può sperimentare, in file separato,
+    tutto quello che abbiamo visto oggi!
+*/
+console.log("HERE",Vue);
+
+Vue.config.devtools=true;
+
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
