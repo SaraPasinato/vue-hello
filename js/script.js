ad4f9c0d5161c3fd75c52a111f90c6ad1f3dc29d
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-27 14:17:04 +0200

    add img: BONUS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-/* Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1,
-   utilizzando le properietà di Vue presenti in data.
+/* 
+   *Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1,
+   *utilizzando le properietà di Vue presenti in data.
    ?Bonus
      Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
    *POST BONUS:
@@ -15,6 +16,7 @@ Vue.config.devtools=true;
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      source: 'https://picsum.photos/200/300'
     }
   })
