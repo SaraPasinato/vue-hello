04e2fb34986d1a1ab6ebebe20f2eb9d39067784a
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-27 14:21:08 +0200

    add css

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
    *Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1,
    *utilizzando le properietà di Vue presenti in data.
    ?Bonus
-     Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
+     ?Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
    *POST BONUS:
     Chi invece è in regola con la consegna degli esercizi può sperimentare, in file separato,
     tutto quello che abbiamo visto oggi!
